da5a5612db0e81a62cbb5c24cb39764d5abd8535
jiaqiyang <jkyyang@foxmail.com>
2023-03-07 14:03:06 +0800

    Universal Update

--- src/main/java/com/example/svr/config/MybatisPlusConfig.java
+++ src/main/java/com/example/svr/config/MybatisPlusConfig.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerIntercept
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+//為了解決bug，在分頁查詢部分
 @Configuration
 public class MybatisPlusConfig {
     @Bean
